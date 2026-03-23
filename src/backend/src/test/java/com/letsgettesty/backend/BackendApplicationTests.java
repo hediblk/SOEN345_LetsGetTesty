9f6e98eeac5a472ce7e4471ff5d148c37926cf4f
likeblossom <pamanderzz@hotmail.com>
2026-03-23 19:04:26 -0400

    fixes

--- src/backend/src/test/java/com/letsgettesty/backend/BackendApplicationTests.java
+++ src/backend/src/test/java/com/letsgettesty/backend/BackendApplicationTests.java
@@ -3,7 +3,8 @@ package com.letsgettesty.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class BackendApplicationTests {
 
 	@Test
